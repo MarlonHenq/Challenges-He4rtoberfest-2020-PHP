698b6f5ca7b8a7cebc64bc1c011bb5fb02f6a7e1
MarlonHenq <marllonsanches@gmail.com>
2020-12-21 00:40:39 -0300

    1-media-alunos

---
+++ 1-media-alunos/media-alunos.php
@@ -0,0 +1,9 @@
+<?php
+
+$nota1 = readline("Digite a primeira nota: ");
+$nota2 = readline("Digite a segunda nota: ");
+
+$soma = $nota1 + $nota2;
+$media = $soma / 2;
+
+echo "A média é: $media\n";
